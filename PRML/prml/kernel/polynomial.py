ff43ae250f567b6a0438740828fd02088ba4c0f6
RL_learning <2890035926@QQ.com>
2018-01-19 16:04:46 +0800

    Create polynomial.py

---
+++ PRML/prml/kernel/polynomial.py
@@ -0,0 +1,41 @@
+# 输入（x, y）数据对，经过多项式计算，就能得到我们想要的核函数值 K（x, y）.
+
+import numpy as np
+from prml.kernel.kernel import Kernel
+
+class PolynomialKernel(Kernel):
+    """
+    Polynomial kernel
+    k(x,y) = (x @ y + c)^M
+    """
+
+    def __init__(self, degree=2, const=0.):
+        """
+        construct Polynomial kernel
+        Parameters
+        ----------
+        const : float
+            a constant to be added
+        degree : int
+            degree of polynomial order
+        """
+        self.const = const
+        self.degree = degree
+
+    def __call__(self, x, y, pairwise=True):
+        """
+        calculate pairwise polynomial kernel
+        Parameters
+        ----------
+        x : (..., ndim) ndarray
+            input
+        y : (..., ndim) ndarray
+            another input with the same shape
+        Returns
+        -------
+        output : ndarray
+            polynomial kernel
+        """
+        if pairwise:
+            x, y = self._pairwise(x, y)
+        return (np.sum(x * y, axis=-1) + self.const) ** self.degree
